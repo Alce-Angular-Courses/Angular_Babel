ff79df8790d08cc228c0a551d6fbfc0e044df037
alce65 <alce65@hotmail.es>
2018-03-05 14:01:31 +0100

    Angular Inicial

---
+++ base-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bbl-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Babel';
+}
